9e5f0e931322730772fdec1a4f9fe1aa0e55d018
Lutz <lutz-peter@imail.de>
2022-06-17 10:17:15 +0200

    Create loadingscreen.c

---
+++ loadingscreen.c
@@ -0,0 +1,56 @@
+modded class LoadingScreen
+{
+	ref TStringArray MyFuckingLoadingScreenArray = {
+		"My_loading_screenshow\\images\\loadingscreen_namalsk_01.edds",
+		"My_loading_screenshow\\images\\loadingscreen_namalsk_02.edds",
+		"My_loading_screenshow\\images\\loadingscreen_namalsk_03.edds"};
+
+	override void Show()
+	{		
+		string preel = MyFuckingLoadingScreenArray.GetRandomElement();
+		m_ImageWidgetBackground.LoadMaskTexture("My_loading_screenshow/data/ls_mask.paa");
+		m_ImageWidgetBackground.LoadImageFile(0, preel);
+		m_ImageLogoMid.Show(false);
+		m_ImageLogoCorner.Show(false);
+		m_ModdedWarning.Show(false); // Show / Hide - Modded version warning // 1.17 OK
+		super.Show();
+	}
+}
+
+modded class LoginTimeBase extends UIScriptedMenu
+{
+	ref TStringArray MyFuckingLoadingScreenArray = {
+		"My_loading_screenshow\\images\\loadingscreen_namalsk_01.edds",
+		"My_loading_screenshow\\images\\loadingscreen_namalsk_02.edds",
+		"My_loading_screenshow\\images\\loadingscreen_namalsk_03.edds"};
+
+	override void Show()
+	{
+		super.Show();
+		if (layoutRoot)
+		{
+			string preel = MyFuckingLoadingScreenArray.GetRandomElement();
+			ImageWidget background = ImageWidget.Cast( layoutRoot.FindAnyWidget("Background"));
+			background.LoadImageFile(0, preel);  
+		}
+	}
+}
+
+modded class LoginQueueBase extends UIScriptedMenu
+{
+	ref TStringArray MyFuckingLoadingScreenArray = {
+		"My_loading_screenshow\\images\\loadingscreen_namalsk_01.edds",
+		"My_loading_screenshow\\images\\loadingscreen_namalsk_02.edds",
+		"My_loading_screenshow\\images\\loadingscreen_namalsk_03.edds"};
+
+	override void Show()
+	{
+		super.Show();
+		if (layoutRoot)
+		{
+			string preel = MyFuckingLoadingScreenArray.GetRandomElement();
+			ImageWidget background = ImageWidget.Cast( layoutRoot.FindAnyWidget("Background"));
+			background.LoadImageFile(0, preel);  
+		}
+	}
+}
